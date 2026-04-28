c7082f0404b3b8798efdbd63b1a15eb0dca22a05
AbdulnabiKR <abdulnabikr94@gmail.com>
2026-04-28 11:17:16 +0530

    Cleaned schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,7 +9,3 @@ CREATE TABLE events (
     time TIME NOT NULL
 );
 
-INSERT INTO events (title, date, time) VALUES ('Meeting', '2026-04-29', '10:00:00');
-DELETE FROM events Where id=8;
-SELECT * FROM events;
-SHOW DATABASES;
